d7df0ada8592568f9e19999353974a0e2d9e26be
Evgeny Zhilkin <zhilkievge@ecs.vuw.ac.nz>
2022-05-17 13:33:16 +1200

    finished turning method, but it needs fine tuning with picking correct coefficients and need the line reader code finished

--- move_demo.cpp
+++ move_demo.cpp
@@ -1,27 +1,60 @@
 
 #include "images.h" 
-int prevVel = 0;
+int prevLeftVel = 0;
+int prevRightVel = 0;
+int forwardVel = 0;
 const int MAX_VEL = 40;
 
-int linear_move(int tarVel){
+// reverse engineered set_motor method, will calculate perfect speed
+// to reach the target speed in 1 step, doesnt work for speeds above 24
+// because that will go over the robots max speed and take more than 1 step
+int get_vel(int tarVel, int prevVel) {
 	int vel = (tarVel - prevVel) / 0.6 + prevVel;
-	while (vel > MAX_VEL || vel < -MAX_VEL) {
+	return vel;
+}
+
+// WARNING use only with target velocity below 24
+int set_vel(int tarVel, int velD){
+	tarVel = (tarVel - velD < -24) ? -24 + velD : tarVel;
+	tarVel = (tarVel > 24 + velD) ? 24 - velD: tarVel;
+	
+	int velLeft = get_vel(tarVel - velD, prevLeftVel);
+	prevLeftVel += velLeft;
+	
+	int velRight = get_vel(tarVel + velD, prevRightVel);
+	prevRightVel += velRight;
+	
+	forwardVel = tarVel;
+	set_motors(velLeft,velRight);
+	return 0;
+}
+/*
+ while (vel > MAX_VEL || vel < -MAX_VEL) {
 		if (vel > 0) set_motors(MAX_VEL,MAX_VEL);
 		else set_motors(-MAX_VEL,-MAX_VEL);
 		prevVel += vel;
+		update_sim(1500);
 		vel = (tarVel - prevVel) / 0.6 + prevVel;
 	}
 	set_motors(vel,vel);
-	return 0;
-}
+	update_sim(1500);
+ **/
 
-int turning_move(int error[], int f_vel, int step) {
-	if (step == 0) linear_move(20);
+int turning_move(int error,int prevError, int f_vel, int step) {
+	// coefficients need tuning
+	const double propGain = 1;
+	const double derivGain = 1;
+	// first step is just forward
+	if (step == 0) set_vel(20, 0);
 	else {
-		
+		int velDiff = error * propGain + (error - prevError) * derivGain;
+		set_vel(forwardVel, velDiff);
 	}
 	return 0;
 } 
+int core() {
+	return 0;
+}
 int main()
 {        
 	std::cout<<"start simulation..."<<std::endl;
