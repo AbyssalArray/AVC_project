662b84c869cf5eb41b0cf13dccabbca6a5795edd
Evgeny Zhilkin <zhilkievge@ecs.vuw.ac.nz>
2022-05-27 18:51:17 +1200

    core working

--- move_demo.cpp
+++ move_demo.cpp
@@ -9,7 +9,6 @@ int prevRightVel = 0;
 int forwardVel = 0;
 const int MAX_VEL = 40;
 int prevError = 0;
-double minCol = 30; // the strength of the colour (out of 255)
 
 // reverse engineered set_motor method, will calculate perfect speed
 // to reach the target speed in 1 step, doesnt work for speeds above 24
@@ -17,21 +16,21 @@ double minCol = 30; // the strength of the colour (out of 255)
 int get_vel(int tarVel, int prevVel)
 {
 	int vel = (tarVel - prevVel) / 0.6 + prevVel;
-	std::cout << "Velocity: " << vel << " | Previous Velocity: " << prevVel << std::endl;
+	std::cout << "velocity, prev velocity :"<<vel << "," << prevVel << std::endl;
 	return vel;
 }
 
 // WARNING use only with target velocity below 24
 int set_vel(int tarVel, int velD)
 {
-	tarVel = (tarVel - velD < -24) ? -24 + velD : tarVel;
-	tarVel = (tarVel > 24 + velD) ? 24 - velD : tarVel;
+	velD = (tarVel - velD < -24) ? 12  : velD;
+	velD = (tarVel + velD> 24 ) ? 12  : velD;
+	
+	int velLeft = get_vel(tarVel + velD, prevLeftVel);
+	prevLeftVel = (tarVel + velD);
 
-	int velLeft = get_vel(tarVel - velD, prevLeftVel);
-	prevLeftVel = (tarVel - velD);
-
-	int velRight = get_vel(tarVel + velD, prevRightVel);
-	prevRightVel = (tarVel + velD);
+	int velRight = get_vel(tarVel - velD, prevRightVel);
+	prevRightVel = (tarVel - velD);
 
 	forwardVel = tarVel;
 	set_motors(velLeft, velRight);
@@ -50,11 +49,11 @@ int set_vel(int tarVel, int velD)
  **/
 int offset_calc()
 {
-	//std::vector<int> index_array; // indexing array from -50 to 49 for inner product calculation
-	//std::vector<int> range_array; // array for range of pixels
+	std::vector<int> index_array; // indexing array from -50 to 49 for inner product calculation
+	std::vector<int> range_array; // array for range of pixels
 	std::vector<int> bW_array;	  // array for black and white pixels
-	//int init = 0;				  // additonal number for inner product
-    int offset = 0;
+	int init = 0;				  // additonal number for inner product
+
 	for (int i = 0; i < camera_image.width; i++)
 	{
 		int r, g, b = 0;
@@ -62,67 +61,66 @@ int offset_calc()
 		g = (int)get_camera_pixel(50, i, 1);
 		b = (int)get_camera_pixel(50, i, 2);
 		int rgbv = (r + g + b) / 3;
-
-		if (rgbv < minCol)
+		if (rgbv < 30)
 		{
 			bW_array.push_back(1); // black
 		}
-		else if (rgbv >= minCol)
+		else if (rgbv >= 30)
 		{
 			bW_array.push_back(0); // white
 		}
-        
-		/*// std::cout<<bW_array.at(i)<<std::endl;
+		// std::cout<<bW_array.at(i)<<std::endl;
+	}
+
+	for (int i = 0; i < camera_image.width; i++)
+	{
 		range_array.push_back(i);
 		// std::cout<<range_array.at(i)<<std::endl;
+	}
+
+	for (int i = 0; i < camera_image.width; i++)
+	{
+
 		int mPosition = camera_image.width / 2;
 		index_array.push_back(i - mPosition);
-		// std::cout << index_array.at(i) << std::endl;
+		// std::cout<<index_array.at(i)<<std::endl;
 	}
-   
 
-	//int offset_calc = std::inner_product(index_array.begin(), index_array.end(), bW_array.begin(), init); // std::accumulate(bW_array.begin(), bW_array.end(), 0);
-	//std::cout << "Offset: " << offset_calc << std::endl;
-	*/
-}
-	for (int i =0; i<camera_image.width; i++){
-	if (bW_array[i]==1){
-	offset = i-50;	
-	}
-}
+	int offset_calc = std::inner_product(index_array.begin(), index_array.end(), bW_array.begin(), init);
+	std::cout << "error: "<<offset_calc << std::endl;
 	bW_array.clear();
-	//range_array.clear();
-	//index_array.clear();
-    std::cout<< offset <<std::endl;
-	return offset;
+	range_array.clear();
+	index_array.clear();
+
+	// return offset calc;
+	return offset_calc;
 }
 
 int turning_move(int error, int prevError, int f_vel, int step)
 {
+
 	// coefficients need tuning
-	const double propGain = 0.125;
-	const double derivGain = 0.125;
+	const double propGain = 0.01;
+	const double derivGain = 0.005;
 	// first step is just forward
 	if (step == 0)
 		set_vel(f_vel, 0);
 	else
 	{
 		int velDiff = error * propGain + (error - prevError) * derivGain;
-		set_vel(forwardVel, velDiff);
+		std::cout<<"vel diff: "<<velDiff<<std::endl;
+		set_vel(f_vel, velDiff);
 	}
 	return 0;
 }
 
 bool pixelHasColour(int colourId, Pixel pixel)
 {
-	double threshold = 1.5; // the amount by which the red, green or blue has to be above the other two
+	
+	double threshold = 1.5;	  // the amount by which the red, green or blue has to be above the other two
+	double minCol = 5; // the strength of the colour (out of 255)
 	double luminosity = (pixel.r + pixel.g + pixel.b) / 3.0;
-
-	/*std::cout << "Red: " << (int)pixel.r << std::endl;
-	std::cout << "Green: " << (int)pixel.g << std::endl;
-	std::cout << "Blue: " << (int)pixel.b << std::endl;
-	std::cout << "Luminosity: " << (int)luminosity << std::endl;*/
-
+	
 	switch (colourId)
 	{
 	case 0: // red
@@ -139,44 +137,52 @@ bool pixelHasColour(int colourId, Pixel pixel)
 		break;
 	case 3: // black
 		if (luminosity < minCol)
-			return true; // if pixel is darker than minCol, pixel is black
+			return true; // if pixel blue is more than luminosity by some threshold and mainly blue, pixel is blue
 		break;
 	}
 	return false;
 }
 
-bool hasColour(int colourId)
+bool hasColour(int colourId, ImagePPM camera)
 {
-	double minColPixels = 20;
-	double numColPixels = 0;
-	if (colourId == 0)
-		std::cout << "New image" << std::endl;
-
-	for (int row = 0; row < camera_image.height; row++)
+	double maxNumberOfColouredPixels = 0;
+	double numberOfColouredPixels = 0;
+	//potential issue in for loop
+	for (int i = 0; i < (int)camera.pixs.size(); i++)
 	{
-		for (int column = 0; column < camera_image.width; column++)
+		if (pixelHasColour(colourId, camera.pixs[i]))
 		{
-			if (pixelHasColour(colourId, get_camera_pixel(row, column)))
+			if (maxNumberOfColouredPixels >= numberOfColouredPixels)
 			{
-				if (minColPixels <= numColPixels)
-					return true;
-				else
-					numColPixels++;
+				return true;
+			}
+			else
+			{
+				numberOfColouredPixels++;
 			}
 		}
 	}
 	return false;
 }
 
+bool detectLine()
+{
+	bool isBlack = hasColour(3,camera_image);
+	
+	return isBlack;
+}
+
 int core()
 {
-	int i = 0;
+	
+	int i = 1;
 	int curError = 0;
-
-	while (!hasColour(0))
-	{
-		if (hasColour(3))
+	
+	while (!hasColour(0, camera_image))
+	{	
+		if (i >= 0)
 		{
+			std::cout<<"forwrads"<<std::endl;
 			curError = offset_calc();
 			turning_move(prevError, curError, 10, i);
 			prevError = curError;
@@ -185,7 +191,9 @@ int core()
 		else
 		{
 			i = 0;
+			curError = offset_calc();
 			turning_move(0, 0, -10, i);
+			prevError = curError;
 			i++;
 		}
 		update_sim(1500);
@@ -196,15 +204,16 @@ int core()
 int main()
 {
 	std::cout << "start simulation..." << std::endl;
+	// init(1698,1018,90*3.14159/180.0); // start of completion
+	// init(1880,640,90*3.14159/180.0);
+	// init(110,160,15*3.14159/180.0); // start of core
 
-	init(110, 160, 0 * M_PI / 180.0); // start of core
-	// init(1880, 640, 90 * M_PI / 180.0); // start of completion
+	init(110, 160, 0 * 3.14159 / 180.0); // start of completion
 
-	core();
-
-	/*offset_calc();
-	turning_move(0, 0, 15, 0);
-	update_sim(1500);*/
-
+		/*offset_calc();
+		turning_move(0, 0, 15, 0);
+		update_sim(1500);*/
+		core();
+	
 	return 0;
 }
